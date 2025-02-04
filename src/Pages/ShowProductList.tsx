0b8e371b4e1f376f59557504d653c9fe5a1c59d0
priyanka807 <priyankacoder11@gmail.com>
2025-02-04 15:20:57 +0530

    Revert "Update ShowProductList.tsx"
    This reverts commit 8ea0f88f20c4f55754ec1ca249af4b5910a64d4d.

--- src/Pages/ShowProductList.tsx
+++ src/Pages/ShowProductList.tsx
@@ -91,7 +91,7 @@ const ShowProductList = () => {
   
   useEffect(() => {
     axios
-      .get(`https://my-json-server.typicode.com/priyanka807/demo/productList`)
+      .get(`https://crudapplication-pld2.vercel.app/productList`)
       .then((response) => {
 
         setProducts(response.data)
@@ -103,7 +103,7 @@ const ShowProductList = () => {
   const accessAuthentication = useCallback(() => {
     const role = localStorage.getItem('role')
   
-    axios.get(`https://my-json-server.typicode.com/priyanka807/demo/roleaccess?menu=productList&role=${role}`)
+    axios.get(`https://crudapplication-pld2.vercel.app/roleaccess?menu=productList&role=${role}`)
 
       .then((res) => {
         if (res.status === 200) {
@@ -148,7 +148,7 @@ const ShowProductList = () => {
             onClick: async () => {
               try {
                 const response = await axios.delete(
-                  `https://my-json-server.typicode.com/priyanka807/demo/productList/${id}`
+                  `https://crudapplication-pld2.vercel.app/productList/${id}`
                 );
                 setProducts((prevStudents) =>
                   prevStudents.filter((product) => product.id !== id)
